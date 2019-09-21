9cc306d9e2b1663adcf9c6f895a72e3aef1a29c8
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-21 18:22:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
 split_string = string.split(" ")
 reversed = []
-split_string.collect do |word|
+split_string.each do |word|
   reversed << word.reverse
 end
 reversed.join(" ")
